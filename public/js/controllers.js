74b14ccbfaf675ff23e2293bdd9769e4d05eb7f5
Raphael Amorim <rapha850@gmail.com>
2014-03-20 00:42:59 -0300

    improving angular base

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,25 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
+  controller('MyCtrl1', [function() {
 
-    $http({
-      method: 'GET',
-      url: '/api/name'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.name = data.name;
-    }).
-    error(function (data, status, headers, config) {
-      $scope.name = 'Error!'
-    });
+  }])
+  .controller('MyCtrl2', [function() {
 
-  }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
-
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
-
-  });
+  }]);
