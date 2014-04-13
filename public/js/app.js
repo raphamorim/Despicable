a82d12032235d75862d58aba344e201d22842036
Raphael Amorim <rapha850@gmail.com>
2014-04-13 00:30:03 -0300

    improve code and add partial to angular

--- public/js/app.js
+++ public/js/app.js
@@ -8,25 +8,3 @@ var despicable = angular.module('despicable', [
     'myApp.directives'
     // ,'ngRoute'
   ]);
-
-// despicable.config(['$routeProvider', function($routeProvider) {
-//     $routeProvider
-//
-//     // route for the signin page
-// 		.when('/signin', {
-// 			templateUrl : 'partials/signin.html',
-// 			controller  : 'signinController'
-// 		})
-//
-// 		// route for the newbie signup page
-// 		.when('/signupNewbie', {
-// 			templateUrl : 'partials/signupNewbie.html',
-// 			controller  : 'newbieController'
-// 		})
-//
-// 		// route for the enterprise signup page
-// 		.when('/signupEnterprise', {
-// 			templateUrl : 'partials/signupEnterprise.html',
-// 			controller  : 'enterpriseController'
-// 		});
-// }]);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,9 +6,11 @@ angular.module('myApp.controllers', []).
 
   controller('mainController', function($scope) {
       $scope.message = 'You\'re in home';
+      $scope.template = '../partials/one';
   })
 
   .controller('aboutController', function($scope) {
       $scope.message = 'What is Despicable? Be Mean, without lose your style.';
-      $scope.message += 'This project use MEAN stack: MongoDB, Express, AngularJS and NodeJS.';
+      $scope.message += 'This project use MEAN stack: Mongoose, Twitter Bootstrap, Express, AngularJS and NodeJS.';
+      $scope.template = '../partials/two';
   });
